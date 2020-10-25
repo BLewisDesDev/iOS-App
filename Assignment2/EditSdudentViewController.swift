56272636a527b5acad43227cf29fcb556b3239cb
Byron <b.lewis.design.development@gmail.com>
2020-10-25 11:28:42 +1100

    Basic flow no logic

---
+++ Assignment2/EditSdudentViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EditSdudentViewController.swift
+//  Assignment2
+//
+//  Created by Collective X on 24/10/20.
+//  Copyright © 2020 Byron. All rights reserved.
+//
+
+import UIKit
+
+class EditSdudentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Assignment2/ViewController.swift
@@ -0,0 +1,14 @@
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var btnList: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
